18c85ca227b9bc839ffc90acb2ea92f2f93e941e
UNANI <coder.unani@gmail.com>
2024-05-21 22:20:35 +0900

    [feat] Model 재구성에 따른 Queryset 리팩토링

--- builder_video/parsers.py
+++ builder_video/parsers.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
 from .utils import html_escape
+from .logger import info_log, error_log
 from .properties import (
     URL_NETFLIX_LOGIN,
     URL_NETFLIX_CONTENT
@@ -53,18 +54,20 @@ class SeleniumParser:
                 self._selenium = webdriver.Chrome(options=options)
                 self._selenium.get(url)
                 self.wait(2)
+                info_log(self.__class__.__name__, "driver is ready")
                 return True
             except Exception as e:
-                print(str(e))
+                error_log(self.__class__.__name__, str(e))
                 return False
         else:
-            print("Not supported driver")
+            error_log(self.__class__.__name__, "Not supported driver")
             return False
     
     def selenium_element(self, type=None, value=None, target=None):
+        info_log(self.__class__.__name__, "selenium_element")
         result = None
         if type not in ["class", "name", "id", "tag"]:
-            print("Not supported type")
+            error_log(self.__class__.__name__, "Not supported type")
             return result
         try:
             if target is None:
@@ -80,10 +83,11 @@ class SeleniumParser:
             else:
                 print("Not supported type")
         except Exception as e:
-            print(str(e))
+            error_log(self.__class__.__name__, str(e))
         return result
         
     def selenium_elements(self, type=None, value=None, target=None):
+        info_log(self.__class__.__name__, "selenium_elements")
         result = None
         try:
             if target == None:
@@ -97,40 +101,45 @@ class SeleniumParser:
             elif type == "tag":
                 result = target.find_elements(by=By.TAG_NAME, value=value)
             else:
-                print("Not supported type")
+                error_log(self.__class__.__name__, "Not supported type")
         except Exception as e:
-            print(str(e))
+            error_log(self.__class__.__name__, str(e))
         return result
 
     def selenium_attribute(self, target, attr):
+        info_log(self.__class__.__name__, "selenium_attribute")
         result = None
         if attr is not None:
             try:
                 result = target.get_attribute(attr)
             except Exception as e:
-                print(str(e))
+                error_log(self.__class__.__name__, str(e))
         return result
     
     def selenium_click(self, target):
+        info_log(self.__class__.__name__, "selenium_click")
         if target is not None:
             try:
                 target.click()
                 self.selenium_wait(2)
                 return True
             except Exception as e:
-                print(str(e))
+                error_log(self.__class__.__name__, str(e))
                 return False
         else:
-            print("Target is None")
+            error_log(self.__class__.__name__, "Target is None")
             return False
     
     def selenium_wait(self, sec):
+        info_log(self.__class__.__name__, "selenium_wait")
         sleep(sec)
 
     def selenium_scroll_move(self, target):
+        info_log(self.__class__.__name__, "selenium_scroll_move")
         target.location_once_scrolled_into_view
 
     def selenium_scroll_down(self):
+        info_log(self.__class__.__name__, "selenium_scroll_down")
         if self._selenium is not None:
             try:
                 # PAGE_DOWN 키를 이용하여 스크롤 다운
@@ -140,12 +149,13 @@ class SeleniumParser:
                 self.selenium_wait(2)
                 return True
             except Exception as e:
-                print(str(e))
+                info_log(self.__class__.__name__, str(e))
                 return False
         else:
             return False
 
     def selenium_scroll_up(self):
+        info_log(self.__class__.__name__, "selenium_scroll_up")
         if self._selenium is not None:
             try:
                 # PAGE_DOWN 키를 이용하여 스크롤 다운
@@ -155,21 +165,24 @@ class SeleniumParser:
                 self.selenium_wait(2)
                 return True
             except Exception as e:
-                print(str(e))
+                info_log(self.__class__.__name__, str(e))
                 return False
         else:
             return False
 
     def selenium_close(self):
-        self._selenium.quit()
-
+        if self._selenium is not None:
+            info_log(self.__class__.__name__, "Selenium driver is closed")
+            self._selenium.quit()
 
 class NetflixParser(BeautyfulSoupParser, SeleniumParser):
     def __init__(self):
         self._soup = None
         self._selenium = None
+        info_log(self.__class__.__name__, "NetflixParser init")
 
     def login_netflix(self, id, pw):
+        info_log(self.__class__.__name__, "Try to login netflix. id={}".format(id))
         try:
             netflix_id = self.selenium_element(type="name", value="userLoginId")
             netflix_pw = self.selenium_element(type="name", value="password")
@@ -178,149 +191,165 @@ class NetflixParser(BeautyfulSoupParser, SeleniumParser):
             netflix_pw.send_keys(Keys.RETURN)
             # 로그인 후 페이지 전환 대기 (3s)
             self.selenium_wait(3)
+            info_log(self.__class__.__name__, "Netflix login success")
             return True
         except Exception as e:
-            print(str(e))
+            error_log(self.__class__.__name__, "Failed netflix login. {}".format(e))
             return False
 
     def get_content_netflix(self, id):
+        info_log(self.__class__.__name__, "Try to import content. id={}".format(id))
         url = URL_NETFLIX_CONTENT + "/" + id
-        with urlopen(url) as response:
-            html = response.read()
-        # BeautySoup을 이용하여 데이터 파싱
-        if html:
+        try:
+            with urlopen(url) as response:
+                html = response.read()
+            # BeautySoup을 이용하여 데이터 파싱
             content = self.parse_content_netflix(html)
+            info_log(self.__class__.__name__, "Content import success. content={}".format(content))
             return content
-        else:
+        except Exception as e:
+            error_log(self.__class__.__name__, "Failed to import content. {}".format(e))
             return None
             
     def get_most_watched(self):
-
-        self.set_selenium(URL_NETFLIX_LOGIN)
-        self.selenium_wait(2)
-        self.login_netflix(getattr(settings, "NETFLIX_ID"), getattr(settings, "NETFLIX_PW"))
-        self.selenium_wait(2)
-
-        # 시리즈 TOP10과 영화 TOP10을 담을 리스트 초기화
-        contents = []
-
-        # 컨텐츠 파싱작업
-        content_rows = self.selenium_elements(type="class", value="lolomoRow")
-        for content_row in content_rows:
-            # 컨텐츠가 화면에 보이도록 스크롤 이동
-            self.selenium_scroll_move(target=content_row)
+        info_log(self.__class__.__name__, "Try to get most watched contents")
+        try:
+            self.set_selenium(URL_NETFLIX_LOGIN)
             self.selenium_wait(2)
-            # 가져온 컨텐츠가 mostWatched 이면 데이터 파싱 진행
-            if self.selenium_attribute(target=content_row, attr="data-list-context") == "mostWatched":
-                # 컨텐츠의 더보기 요소를 클릭하여 추가 요소가 렌더링 되도록 함
-                handleNext = self.selenium_element(type="class", value="handleNext", target=content_row)
-                self.selenium_click(target=handleNext)
+            self.login_netflix(getattr(settings, "NETFLIX_ID"), getattr(settings, "NETFLIX_PW"))
+            self.selenium_wait(2)
+
+            # 시리즈 TOP10과 영화 TOP10을 담을 리스트 초기화
+            contents = []
+
+            # 컨텐츠 파싱작업
+            content_rows = self.selenium_elements(type="class", value="lolomoRow")
+            for content_row in content_rows:
+                # 컨텐츠가 화면에 보이도록 스크롤 이동
+                self.selenium_scroll_move(target=content_row)
                 self.selenium_wait(2)
-                # 컨텐츠 타이틀
-                content_row_title = self.selenium_element(type="class", value="rowHeader", target=content_row).text
-                # content_row 에 담긴 데이터 리스트
-                content_row_items = self.selenium_elements(type="class", value="slider-item", target=content_row)
-                # 컨텐츠별 프리픽스 설정
-                type = ""
-                if content_row_title == "오늘 대한민국의 TOP 10 시리즈":
-                    type = "11"
-                elif content_row_title == "오늘 대한민국의 TOP 10 영화":
-                    type = "10"
-                # 컨텐츠 갯수만큼 반복
-                for item in content_row_items:
-                    # 필수 정보 파싱
-                    rank = self.selenium_element(type="tag", value="svg", target=item)
-                    rank = self.selenium_attribute(target=rank, attr="id").split("-")[1]
-                    rank = int(rank)
-                    link = self.selenium_element(type="tag", value="a", target=item)
-                    link = self.selenium_attribute(target=link, attr="href")
-                    platform_id = link.split("?")[0].split("/watch/")[1]
-                    thumbnail = self.selenium_element(type="tag", value="img", target=item)
-                    thumbnail = self.selenium_attribute(target=thumbnail, attr="src")
-                    
-                    new_content = self.get_content_netflix(platform_id)
-                    new_content['rank'] = rank
-                    new_content['thumbnails'].append({"type": "10", "thumbnail": thumbnail, "extension": "", "size": 0})
+                # 가져온 컨텐츠가 mostWatched 이면 데이터 파싱 진행
+                if self.selenium_attribute(target=content_row, attr="data-list-context") == "mostWatched":
+                    # 컨텐츠의 더보기 요소를 클릭하여 추가 요소가 렌더링 되도록 함
+                    handleNext = self.selenium_element(type="class", value="handleNext", target=content_row)
+                    self.selenium_click(target=handleNext)
+                    self.selenium_wait(2)
+                    # 컨텐츠 타이틀
+                    content_row_title = self.selenium_element(type="class", value="rowHeader", target=content_row).text
+                    # content_row 에 담긴 데이터 리스트
+                    content_row_items = self.selenium_elements(type="class", value="slider-item", target=content_row)
+                    # 컨텐츠별 프리픽스 설정
+                    type = ""
+                    if content_row_title == "오늘 대한민국의 TOP 10 시리즈":
+                        type = "11"
+                    elif content_row_title == "오늘 대한민국의 TOP 10 영화":
+                        type = "10"
+                    # 컨텐츠 갯수만큼 반복
+                    for item in content_row_items:
+                        # 필수 정보 파싱
+                        rank = self.selenium_element(type="tag", value="svg", target=item)
+                        rank = self.selenium_attribute(target=rank, attr="id").split("-")[1]
+                        rank = int(rank)
+                        link = self.selenium_element(type="tag", value="a", target=item)
+                        link = self.selenium_attribute(target=link, attr="href")
+                        platform_id = link.split("?")[0].split("/watch/")[1]
+                        thumbnail = self.selenium_element(type="tag", value="img", target=item)
+                        thumbnail = self.selenium_attribute(target=thumbnail, attr="src")
+                        
+                        new_content = self.get_content_netflix(platform_id)
+                        new_content['rank'] = rank
+                        new_content['thumbnail'].append({"type": "10", "url": thumbnail, "extension": "", "size": 0})
 
-                    # 중복데이터 방지
-                    if new_content not in contents:
-                            contents.append(new_content)
-            # 가져온 컨텐츠가 mostWatched가 아니면 continue
-            else:
-                continue
+                        # 중복데이터 방지
+                        if new_content not in contents:
+                                contents.append(new_content)
+                # 가져온 컨텐츠가 mostWatched가 아니면 continue
+                else:
+                    continue
 
-        # rank 순으로 정렬
-        contents = sorted(contents, key=lambda k: (k['type'], k['rank']))
+            # rank 순으로 정렬
+            contents = sorted(contents, key=lambda k: (k['type'], k['rank']))
 
-        # selenium 종료
-        self.selenium_close()
-        # 파싱한 데이터 리스트 형태로 리턴
-        return contents
+            info_log(self.__class__.__name__, "Most watched contents parsing success")
+            # 파싱한 데이터 리스트 형태로 리턴
+            return contents
+        except Exception as e:
+            error_log(self.__class__.__name__, "Failed to most watched contents parsing. {}".format(e))
+            return None
+        finally:
+            self.selenium_close()
     
     def parse_content_netflix(self, html):
-        self.set_beautyfulsoup(html)        
-        schema = self.soup_element(tag="script", attrs={"type": "application/ld+json"})
-        schema = schema.text.strip()
-        schema_to_dict = eval(schema)
+        info_log(self.__class__.__name__, "Try to parse netflix content")
+        try:
+            self.set_beautyfulsoup(html)        
+            schema = self.soup_element(tag="script", attrs={"type": "application/ld+json"})
+            schema = schema.text.strip()
+            schema_to_dict = eval(schema)
 
-        content = dict()
-        # type 추출
-        content['type'] = "" 
-        if schema_to_dict['@type'] == "Movie":
-            content['type'] = "10"
-        elif schema_to_dict['@type'] == "TVSeries":
-            content['type'] = "11"
-        content['title'] = schema_to_dict['name'] # title 추출
-        content['synopsis'] = schema_to_dict['description'] # synopsis 추출
-        content['platform_code'] = "10" # Netflix platform_code
-        content['platform_id'] = schema_to_dict['url'].split("/")[-1] # platform_id 추출
-        content['notice_age'] = schema_to_dict['contentRating'] # 연령고지 추출
-        # Thumbnail 이미지 추출
-        content['thumbnails'] = [
-            {"type": "11", "thumbnail": schema_to_dict['image'], "extension": "", "size": 0}
-        ]
-        # watch url 추출
-        content['watchs'] = [
-            {"type": "10", "url": schema_to_dict['url']}  
-        ]
-        # 장르 추출
-        content['genres'] = []
-        genres = self.soup_elements(tag="a", classname="item-genres")
-        for genre in genres:
-            content['genres'].append({"name": genre.text.strip()})
-        # 출연진 추출
-        content['actors'] = []
-        actor_count = 0
-        for actor in schema_to_dict['actors']:
-            type = "10"
-            if actor_count > 1:
-                type = "11"
-            content['actors'].append({"type": type, "name": actor['name'], "role": "", "picture": "", "profile": ""})
-            actor_count += 1
-        # 제작진 초기화
-        content['staffs'] = []
-        # 감독 추출
-        for director in schema_to_dict['director']:
-            content['staffs'].append({"type": "10", "name": director['name'], "picture": "", "profile": ""})
-        # 제작진 추출
-        for creator in schema_to_dict['creator']:
-            content['staffs'].append({"type": "11", "name": creator['name'], "picture": "", "profile": ""})
-        # 파싱 범위 축소
-        self._soup = self.soup_element(tag="div", classname="hero-container")
-        # release 추출
-        content['release'] = self.soup_element(tag="span", classname="item-year")
-        if content['release'] is not None:
-            content['release'] = content['release'].text.strip()
-        # 상영시간 추출
-        content['runtime'] = self.soup_element(tag="span", classname="item-runtime", attrs={"data-uia": "item-runtime"})
-        if content['runtime'] is not None:
-            content['runtime'] = content['runtime'].text.strip()
-        
-        self.soup_close()
-        print(content)
-        return content
-    
+            content = dict()
+            # type 추출
+            content['type'] = "" 
+            if schema_to_dict['@type'] == "Movie":
+                content['type'] = "10"
+            elif schema_to_dict['@type'] == "TVSeries":
+                content['type'] = "11"
+            content['title'] = schema_to_dict['name'] # title 추출
+            content['synopsis'] = schema_to_dict['description'] # synopsis 추출
+            content['platform_code'] = "10" # Netflix platform_code
+            content['platform_id'] = schema_to_dict['url'].split("/")[-1] # platform_id 추출
+            content['notice_age'] = schema_to_dict['contentRating'] # 연령고지 추출
+            # Thumbnail 이미지 추출
+            content['thumbnail'] = [
+                {"type": "11", "url": schema_to_dict['image'], "extension": "", "size": 0}
+            ]
+            # watch url 추출
+            content['watch'] = [
+                {"type": "10", "url": schema_to_dict['url']}  
+            ]
+            # 장르 추출
+            content['genre'] = []
+            genres = self.soup_elements(tag="a", classname="item-genres")
+            for genre in genres:
+                content['genre'].append({"name": genre.text.strip()})
+            # 출연진 추출
+            content['actor'] = []
+            actor_count = 0
+            for actor in schema_to_dict['actors']:
+                type = "10"
+                if actor_count > 1:
+                    type = "11"
+                content['actor'].append({"type": type, "name": actor['name'], "role": "", "picture": "", "profile": ""})
+                actor_count += 1
+            # 제작진 초기화
+            content['staff'] = []
+            # 감독 추출
+            for director in schema_to_dict['director']:
+                content['staff'].append({"type": "10", "name": director['name'], "picture": "", "profile": ""})
+            # 제작진 추출
+            for creator in schema_to_dict['creator']:
+                content['staff'].append({"type": "11", "name": creator['name'], "picture": "", "profile": ""})
+            # 파싱 범위 축소
+            self._soup = self.soup_element(tag="div", classname="hero-container")
+            # release 추출
+            content['release'] = self.soup_element(tag="span", classname="item-year")
+            if content['release'] is not None:
+                content['release'] = content['release'].text.strip()
+            # 상영시간 추출
+            content['runtime'] = self.soup_element(tag="span", classname="item-runtime", attrs={"data-uia": "item-runtime"})
+            if content['runtime'] is not None:
+                content['runtime'] = content['runtime'].text.strip()
+            
+            info_log(self.__class__.__name__, "Netflix content parsing success")
+            return content
+        except Exception as e:
+            error_log(self.__class__.__name__, "Failed to parse content. {}".format(e))
+            return None
+        finally:
+            self.soup_close()
+
     def close(self):
+        info_log(self.__class__.__name__, "Close NetflixParser")
         if self._soup is not None:
             self.soup_close()
         if self._selenium is not None:
